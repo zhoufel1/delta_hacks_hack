16871d96648397835d6bd48c05777e5f58b73bf3
Felix <felix.rn.zhou@gmail.com>
2020-01-26 02:55:07 -0500

    Remove websocket credential

--- credentials.py
+++ credentials.py
@@ -4,7 +4,6 @@ credentials = {
         "uaa": "https://auth.aa.cityiq.io",
         "metadata": "https://hamilton.cityiq.io/api/v2/metadata",
         "event": "https://hamilton.cityiq.io/api/v2/event",
-        "websocket": "wss://{yourWebSocketURL}",
         "developer": "Hackathon.CITM.Hamilton:Wm,yb&G`KB\\2}d<s",
         "parking": "HAMILTON-IE-PARKING",
         "environment": "HAMILTON-IE-ENVIRONMENTAL",
